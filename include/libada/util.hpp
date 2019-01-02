43a1fccb5c718692985a7d66451835b9f6546948
Gilwoo Lee <gilwoo301@gmail.com>
2019-01-02 13:23:16 -0800

    Add util

---
+++ include/libada/util.hpp
@@ -0,0 +1,64 @@
+#ifndef LIBADA_UTIL_HPP_
+#define LIBADA_UTIL_HPP_
+
+#include <Eigen/Dense>
+#include <aikido/trajectory/Interpolated.hpp>
+#include <aikido/trajectory/Spline.hpp>
+#include <aikido/trajectory/Trajectory.hpp>
+#include <boost/program_options.hpp>
+#include <ros/ros.h>
+
+namespace ada {
+namespace util {
+
+/// Displays a message and waits for the user to press the enter key
+/// \param[in] The message to display.
+/// \return False if the user entered 'n'.
+bool waitForUser(const std::string& msg);
+
+/// Loads and returns a ros parameter.
+/// Throws a runtime_error if the parameter is not set.
+/// \param[in] paramName The name of the parameter.
+/// \param[in] nodeHandle Handle of the ros node.
+/// \return The value of the ros parameter.
+template <class T>
+T getRosParam(const std::string& paramName, const ros::NodeHandle& nh);
+
+/// Convenience function to create an Eigen Isometry3D based on position and
+/// rotation.
+/// \param[in] x,y,z Position
+/// \param[in] roll,pitch,yaw Rotation
+/// \return The transform.
+Eigen::Isometry3d createIsometry(
+    double x,
+    double y,
+    double z,
+    double roll = 0,
+    double pitch = 0,
+    double yaw = 0);
+
+/// Convenience function to create an Eigen Isometry3D based on position and
+/// rotation.
+/// \param[in] vec Positi and rotation in a vector like this:
+/// [x,y,z,roll,pitch,yaw]
+/// \return The transform.
+Eigen::Isometry3d createIsometry(std::vector<double> vec);
+
+/// Convenience function to create the Bw Matrix that is needed for TSRs more
+/// easily.
+/// TODO: Docstring.
+Eigen::MatrixXd createBwMatrixForTSR(
+    double horizontalTolerance,
+    double verticalTolerance,
+    double yawMin,
+    double yawMax);
+
+aikido::trajectory::UniqueSplinePtr posePostprocessingForSO2(
+    const aikido::trajectory::Spline& input);
+
+} // namespace util
+} // namespace ada
+
+#include "detail/util-impl.hpp"
+
+#endif
---
+++ src/util.cpp
@@ -0,0 +1,110 @@
+#include "libada/util.hpp"
+
+#include <aikido/common/Spline.hpp>
+#include <aikido/distance/DistanceMetric.hpp>
+#include <aikido/distance/defaults.hpp>
+#include <aikido/statespace/CartesianProduct.hpp>
+#include <aikido/statespace/dart/MetaSkeletonStateSpace.hpp>
+#include <aikido/trajectory/Interpolated.hpp>
+#include <aikido/trajectory/util.hpp>
+
+#include <dart/common/StlHelpers.hpp>
+
+namespace ada {
+namespace util {
+
+//==============================================================================
+bool waitForUser(const std::string& msg)
+{
+  ROS_INFO((msg + " Press [ENTER]").c_str());
+  char input = ' ';
+  std::cin.get(input);
+  return input != 'n';
+}
+
+//==============================================================================
+Eigen::Isometry3d createIsometry(
+    double x, double y, double z, double roll, double pitch, double yaw)
+{
+  Eigen::Isometry3d isometry = Eigen::Isometry3d::Identity();
+  isometry.translation() = Eigen::Vector3d(x, y, z);
+  Eigen::Matrix3d rotation;
+  rotation = Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ())
+             * Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY())
+             * Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX());
+  isometry.linear() = rotation;
+
+  return isometry;
+}
+
+//==============================================================================
+Eigen::Isometry3d createIsometry(std::vector<double> vec)
+{
+  if (vec.size() < 6)
+  {
+    throw std::runtime_error("Vector size to small: " + vec.size());
+  }
+  return createIsometry(vec[0], vec[1], vec[2], vec[3], vec[4], vec[5]);
+}
+
+//==============================================================================
+Eigen::MatrixXd createBwMatrixForTSR(
+    double horizontalTolerance,
+    double verticalTolerance,
+    double yawMin,
+    double yawMax)
+{
+  Eigen::MatrixXd bw = Eigen::Matrix<double, 6, 2>::Zero();
+  bw(0, 0) = -horizontalTolerance;
+  bw(0, 1) = horizontalTolerance;
+  bw(1, 0) = -horizontalTolerance;
+  bw(1, 1) = horizontalTolerance;
+  bw(2, 0) = -verticalTolerance;
+  bw(2, 1) = verticalTolerance;
+  bw(5, 0) = yawMin;
+  bw(5, 1) = yawMax;
+  return bw;
+}
+
+//==============================================================================
+aikido::trajectory::UniqueSplinePtr posePostprocessingForSO2(
+    const aikido::trajectory::Spline& spline)
+{
+
+  auto tmpState = spline.getStateSpace()->createState();
+  Eigen::VectorXd tmpVec(spline.getStateSpace()->getDimension());
+
+  std::vector<Eigen::VectorXd> waypoints;
+  spline.getWaypoint(0, tmpState);
+  spline.getStateSpace()->logMap(tmpState, tmpVec);
+  waypoints.push_back(tmpVec);
+
+  auto interpolator
+      = std::make_shared<aikido::statespace::GeodesicInterpolator>(
+          spline.getStateSpace());
+
+  for (std::size_t i = 0; i < spline.getNumWaypoints() - 1; ++i)
+  {
+    auto state = spline.getStateSpace()->createState();
+    spline.getStateSpace()->expMap(tmpVec, tmpState);
+    spline.getWaypoint(i + 1, state);
+    auto diff = interpolator->getTangentVector(tmpState, state);
+    tmpVec += diff;
+    waypoints.push_back(tmpVec);
+  }
+
+  auto interpolated = std::make_shared<aikido::trajectory::Interpolated>(
+      spline.getStateSpace(), interpolator);
+
+  auto newState = spline.getStateSpace()->createState();
+  for (size_t i = 0; i < waypoints.size(); i++)
+  {
+    spline.getStateSpace()->expMap(waypoints[i], newState);
+    interpolated->addWaypoint(i, newState);
+  }
+
+  return aikido::trajectory::convertToSpline(*interpolated);
+}
+
+} // namespace util
+} // namespace ada
