6a300e0f66d38cab06af90bcb09a2e6bc72ec5fb
sniyaz <sniyaz@berkeley.edu>
2021-03-27 21:09:40 -0700

    Finish rewriting dart_bindings.cpp to remove lambdas.

--- python/adapy/dart_bindings.cpp
+++ python/adapy/dart_bindings.cpp
@@ -12,19 +12,49 @@ namespace py = pybind11;
 //==============================================================================
 // NOTE: These functions define the Python API for Skeleton.
 
-std::string get_name(dart::dynamics::Skeleton* self)
+std::string skeleton_get_name(dart::dynamics::Skeleton* self)
 {
   return self->getName();
 }
 
-Eigen::VectorXd get_positions(dart::dynamics::Skeleton* self)
+Eigen::VectorXd skeleton_get_positions(dart::dynamics::Skeleton* self)
 {
   return self->getPositions();
 }
 
-int get_num_joints(dart::dynamics::Skeleton* self)
+int skeleton_get_num_joints(dart::dynamics::Skeleton* self)
 {
   return self->getNumJoints();
+}
+
+//==============================================================================
+// NOTE: These functions define the Python API for MetaSkeleton.
+
+std::string metaskeleton_get_name(dart::dynamics::MetaSkeleton* self)
+{
+  return self->getName();
+}
+
+Eigen::VectorXd metaskeleton_get_positions(dart::dynamics::MetaSkeleton* self)
+{
+  return self->getPositions();
+}
+
+int metaskeleton_get_num_joints(dart::dynamics::MetaSkeleton* self)
+{
+  return self->getNumJoints();
+}
+
+dart::math::LinearJacobian get_linear_jacobian(
+    dart::dynamics::MetaSkeleton* self, const dart::dynamics::BodyNode* _node)
+{
+  return self->getLinearJacobian(_node);
+}
+
+dart::math::Jacobian get_jacobian(
+    dart::dynamics::MetaSkeleton* self, const dart::dynamics::BodyNode* _node)
+{
+  return self->getJacobian(_node);
 }
 
 //==================================DART========================================
@@ -34,38 +64,15 @@ void Dart(pybind11::module& m)
   py::class_<
       dart::dynamics::Skeleton,
       std::shared_ptr<dart::dynamics::Skeleton>>(m, "Skeleton")
-      .def("get_name", get_name)
-      .def("get_positions", get_positions)
-      .def("get_num_joints", get_num_joints);
+      .def("get_name", skeleton_get_name)
+      .def("get_positions", skeleton_get_positions)
+      .def("get_num_joints", skeleton_get_num_joints);
   py::class_<
       dart::dynamics::MetaSkeleton,
       std::shared_ptr<dart::dynamics::MetaSkeleton>>(m, "MetaSkeleton")
-      .def(
-          "get_name",
-          [](dart::dynamics::MetaSkeleton* self) -> std::string {
-            return self->getName();
-          })
-      .def(
-          "get_positions",
-          [](dart::dynamics::MetaSkeleton* self) -> Eigen::VectorXd {
-            return self->getPositions();
-          })
-      .def(
-          "get_num_joints",
-          [](dart::dynamics::MetaSkeleton* self) -> int {
-            return self->getNumJoints();
-          })
-      .def(
-          "get_linear_jacobian",
-          [](dart::dynamics::MetaSkeleton* self,
-             const dart::dynamics::BodyNode* _node)
-              -> dart::math::LinearJacobian {
-            return self->getLinearJacobian(_node);
-          })
-      .def(
-          "get_jacobian",
-          [](dart::dynamics::MetaSkeleton* self,
-             const dart::dynamics::BodyNode* _node) -> dart::math::Jacobian {
-            return self->getJacobian(_node);
-          });
+      .def("get_name", metaskeleton_get_name)
+      .def("get_positions", metaskeleton_get_positions)
+      .def("get_num_joints", metaskeleton_get_num_joints)
+      .def("get_linear_jacobian", get_linear_jacobian)
+      .def("get_jacobian", get_jacobian);
 }
