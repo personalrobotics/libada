0e00bb97e79ee5cb46bd986acfc97ffe629f2f46
Ethan Gordon <ekgordon@cs.washington.edu>
2021-05-06 16:04:09 -0700

    Add Python installation

--- .ci/install_linux_catkin.sh
+++ .ci/install_linux_catkin.sh
@@ -7,3 +7,10 @@ cp -r "${GITHUB_WORKSPACE}" src
 ./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}" ${REQUIRED_ONLY}
 
 $SUDO apt-get install -y clang-format-6.0
+
+# Right now, build-test always builds adapy on focal
+$SUDO apt-get -y install python3-dev python3-numpy
+$SUDO apt-get -y install python3-pip -y
+$SUDO pip3 install pytest -U
+$SUDO apt-get -y install pybind11-dev python3 libpython3-dev python3-pytest \
+  python3-distutils
