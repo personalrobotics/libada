dd0a7e19296abdc7fd134f1541d17a217876e01f
Amal Nanavati <amaln@uw.edu>
2021-10-26 20:25:19 -0700

    Added a script for offline calibration, addressed comments
    - Added a script and launchfile for offline calibration
    - Added a `use_apriltag_calib` argument to `default.launch`
    - Made the variable/parameters names and comments clearer when referring to frames
    - Formatted scripts to PEP8, using `autopep8`
    - Added documentation for both the offline and real-time calibrations
    - Addressed other PR comments

--- scripts/camera_calibration_tag.py
+++ scripts/camera_calibration_tag.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
+# This file launches a node that continuously computes the transform from the
+# camera to the specified AprilTag, and republishes that transform to attach
+# the camera to the robot's TF tree.
 import rospy
-import math
 import tf
-import geometry_msgs.msg
 
 if __name__ == '__main__':
     rospy.init_node('camera_calibration_tag')
@@ -12,25 +13,29 @@ if __name__ == '__main__':
     broadcaster = tf.TransformBroadcaster()
 
     # Load the static transforms
-    # robot_to_tag_trans = rospy.get_param("robot_to_tag_transform/translation")
-    # robot_to_tag_rot = rospy.get_param("robot_to_tag_transform/rotation")
-    # robot_to_tag_parent = rospy.get_param("robot_to_tag_transform/parent_frame")
-    # robot_to_tag_child = rospy.get_param("robot_to_tag_transform/child_frame")
-    tag_to_camera_default_trans = rospy.get_param("~tag_to_camera_default_transform/translation")
-    tag_to_camera_default_rot = rospy.get_param("~tag_to_camera_default_transform/rotation")
-    tag_to_camera_default_parent = rospy.get_param("~tag_to_camera_default_transform/parent_frame")
-    tag_to_camera_default_child = rospy.get_param("~tag_to_camera_default_transform/child_frame")
-    deteced_tag_frame = rospy.get_param("~apriltag_detector/standalone_tags")[0]["name"]
+    tag_to_camera_trans = rospy.get_param(
+        "~tag_to_camera_default_transform/translation")
+    tag_to_camera_rot = rospy.get_param(
+        "~tag_to_camera_default_transform/rotation")
+    camera_frame = rospy.get_param("~camera_frame")
+    fixed_tag_frame = rospy.get_param("~fixed_tag_frame")
+    deteced_tag_frame = rospy.get_param(
+        "~apriltag_detector/standalone_tags")[0]["name"]
 
     rate = rospy.Rate(30.0)
     while not rospy.is_shutdown():
         try:
-            transform = listener.lookupTransform(deteced_tag_frame, tag_to_camera_default_child, rospy.Time(0))
+            # Get the camera's pose in the tag's frame
+            transform = listener.lookupTransform(
+                deteced_tag_frame, camera_frame, rospy.Time(0))
             tag_to_camera_trans, tag_to_camera_rot = transform
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            tag_to_camera_trans = tag_to_camera_default_trans
-            tag_to_camera_rot = tag_to_camera_default_rot
+            # Use the most recent transformation
+            pass
 
-        # Broadcast the 
-        broadcaster.sendTransform(tag_to_camera_trans, tag_to_camera_rot, rospy.Time.now(), tag_to_camera_default_child, tag_to_camera_default_parent)
+        # Broadcast the transform from the tag to the camera, connecting the
+        # camera to the robot's TF tree
+        broadcaster.sendTransform(tag_to_camera_trans, tag_to_camera_rot,
+                                  rospy.Time.now(), camera_frame, fixed_tag_frame)
+
         rate.sleep()
